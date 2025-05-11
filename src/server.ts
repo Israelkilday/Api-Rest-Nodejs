41b9a8259abc3388bada6c11a6d4649d7ba85bee
Israel Kilday <israel.kilday@yahoo.com.br>
2025-05-11 00:00:30 -0300

    feat: adds  global middleware

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,12 @@ import { transactionsRoutes } from "./routes/transactions";
 import cookie from "@fastify/cookie";
 
 const app = fastify();
+
 app.register(cookie);
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
+app.addHook("preHandler", async (request, reply) => {
+  console.log(`[${request.method}]${request.url}`);
+});
 app.register(transactionsRoutes, {
   prefix: "transactions",
 });

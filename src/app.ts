0cede9deebf306d9116ba0bf8240b089b6e9f0c5
Israel Kilday <israel.kilday@yahoo.com.br>
2025-05-11 01:34:16 -0300

    feat: start file  app.ts for tests

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import fastify from "fastify";
+import { transactionsRoutes } from "./routes/transactions";
+import fastifyCookie from "@fastify/cookie";
+
+export const app = fastify();
+
+app.register(fastifyCookie);
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
+app.addHook("preHandler", async (request, reply) => {
+  console.log(`[${request.method}]${request.url}`);
+});
+app.register(transactionsRoutes, {
+  prefix: "transactions",
+});

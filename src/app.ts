37f1b044e59b04e18ac6c7cb760bb4c0f17d7d22
Israel Kilday <israel.kilday@yahoo.com.br>
2025-05-12 22:28:12 -0300

    feat: create app.ts file to separate server creation logic

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,9 @@ export const app = fastify();
 
 app.register(fastifyCookie);
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
-app.addHook("preHandler", async (request, reply) => {
-  console.log(`[${request.method}]${request.url}`);
-});
+// app.addHook("preHandler", async (request, reply) => {
+//   console.log(`[${request.method}]${request.url}`);
+// });
 app.register(transactionsRoutes, {
   prefix: "transactions",
 });

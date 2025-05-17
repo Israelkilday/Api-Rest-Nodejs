e83a1cc781cf147c4aa236f43a25c8ed6fc5aeb0
Israel Kilday <israel.kilday@yahoo.com.br>
2025-05-17 18:52:43 -0300

    refactor: delete comments

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,6 @@ import fastifyCookie from "@fastify/cookie";
 export const app = fastify();
 
 app.register(fastifyCookie);
-// eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
-// app.addHook("preHandler", async (request, reply) => {
-//   console.log(`[${request.method}]${request.url}`);
-// });
 app.register(transactionsRoutes, {
   prefix: "transactions",
 });

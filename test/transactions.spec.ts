6a3935b11b9699a09d9ce70e9f75726c46781f08
Israel Kilday <israel.kilday@yahoo.com.br>
2025-06-19 21:23:01 -0300

    test: ensure taht the user searches for a tarnsaction by id

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -54,4 +54,39 @@ describe("Transactions routes", () => {
       }),
     ]);
   });
+
+  it("Should be able to get a specific transaction", async () => {
+    const createTransactionResponse = await request(app.server)
+      .post("/transactions")
+      .send({
+        title: "New transaction",
+        amount: 5000,
+        type: "credit",
+      });
+
+    const cookies = createTransactionResponse.get("Set-Cookie");
+
+    if (!cookies) {
+      throw new Error("No cookies returned from login");
+    }
+
+    const listTransactionsResponse = await request(app.server)
+      .get("/transactions")
+      .set("Cookie", cookies)
+      .expect(200);
+
+    const transactionsId = listTransactionsResponse.body.transactions[0].id;
+
+    const getTransactionsResponse = await request(app.server)
+      .get(`/transactions${transactionsId}`)
+      .set("Cookie", cookies)
+      .expect(200);
+
+    expect(getTransactionsResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: "New transaction",
+        amount: 5000,
+      })
+    );
+  });
 });
